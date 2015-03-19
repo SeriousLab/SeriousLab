e6c7197e2d2eb355cc66ebe5b4861af4b8b2bab3
SeriousLab <dominic.sirius@gmail.com>
2015-03-19 15:56:58 +0800

    add game play function
    added game play function making this thing workable.

--- MineSweep/Minesweep.cpp
+++ MineSweep/Minesweep.cpp
@@ -14,110 +14,132 @@ void printMap();
 bool markIndicator();
 bool currentPosition(int x, int y, WORD color);
 void openSurrand(int x, int y);
-void printOpenedMap();
+void gameOver(int x, int y);
+void printCurrentCell(int x, int y);
+void gamePlay();
 
-const int nMine = 50;
+
+const int nMine = 80;
 const int xM[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
 const int yM[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 int counter = 0;
 CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
 int mineCount = 0;
+HANDLE hStdin;
+INPUT_RECORD irInBuf[128];
+DWORD cNumRead;
+DWORD saveOldMode;
+COORD exitCord = { 0, 20 };
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	srand((unsigned int)time(&pTime));
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	GetConsoleMode(hStdin, &saveOldMode);
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
 	generateMap();
+	SetConsoleMode(hStdin, ENABLE_MOUSE_INPUT);
 	markIndicator();
+	for (int x = 0; x < 20; x++)
+	{
+		for (int y = 0; y < 26; y++)
+		{
+			printf("■");
+		}
+		printf("\n");
+	}
+	gamePlay();
 	//printMap();
-	openSurrand(0, 0);
-	printOpenedMap();
+
+
 	return 0;
 }
 
 void generateMap(){
 	for (int n = 1; n < nMine; n++)
 	{
-		mineMap[rand() % 18 + 1][rand() % 24 + 1] = 99;         //地图里随机99个位置生成雷，小概率情况下会随机到相同的位置，少一俩雷将就玩吧；
+		mineMap[rand() % 20][rand() % 26] = 99;         //地图里随机99个位置生成雷，小概率情况下会随机到相同的位置，少一俩雷将就玩吧；
 	}
 }
 
-void printMap(){
-	for (int x = 0; x < 20; x++)
-	{
-		for (int y = 0; y < 26; y++)
-		{
-			if (mineMap[x][y] == 0)
-			{
-				currentPosition(y, x, csbiInfo.wAttributes);
-				printf("■");
-			}
-			if ((mineMap[x][y] >= 1) && (mineMap[x][y] <= 8))
-			{
-				switch (mineMap[x][y])
-				{
-				case 1:
-				{
-						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
-						  printf("１");
-						  break;
-				}
-				case 2:
-				{
-						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-						  printf("２");
-						  break;
-				}
-				case 3:
-				{
-						  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-						  printf("３");
-						  break;
-				}
-				case 4:
-				{
-						  currentPosition(y, x, FOREGROUND_RED | FOREGROUND_INTENSITY);
-						  printf("４");
-						  break;
-				}
-				case 5:
-				{
-						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-						  printf("５");
-						  break;
-				}
-				case 6:
-				{
-						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
-						  printf("６");
-						  break;
-				}
-				case 7:
-				{
-						  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
-						  printf("７");
-						  break;
-				}
-				case 8:
-				{
-						  currentPosition(y, x, csbiInfo.wAttributes);
-						  printf("８");
-						  break;
-				}
-				default:
-					break;
-				}
-			}
-			if (mineMap[x][y] == 99)
-			{
-				currentPosition(y, x, csbiInfo.wAttributes);
-				//printf("¤");
-				printf("■");
-			}
-		}
-		printf("\n");
-	}
-}
+//测试地雷标记输出
+//void printMap(){
+//	for (int x = 0; x < 20; x++)
+//	{
+//		for (int y = 0; y < 26; y++)
+//		{
+//			if (mineMap[x][y] == 0)
+//			{
+//				currentPosition(y, x, csbiInfo.wAttributes);
+//				printf("■");
+//			}
+//			if ((mineMap[x][y] >= 1) && (mineMap[x][y] <= 8))
+//			{
+//				switch (mineMap[x][y])
+//				{
+//				case 1:
+//				{
+//						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+//						  printf("１");
+//						  break;
+//				}
+//				case 2:
+//				{
+//						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+//						  printf("２");
+//						  break;
+//				}
+//				case 3:
+//				{
+//						  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+//						  printf("３");
+//						  break;
+//				}
+//				case 4:
+//				{
+//						  currentPosition(y, x, FOREGROUND_RED | FOREGROUND_INTENSITY);
+//						  printf("４");
+//						  break;
+//				}
+//				case 5:
+//				{
+//						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+//						  printf("５");
+//						  break;
+//				}
+//				case 6:
+//				{
+//						  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
+//						  printf("６");
+//						  break;
+//				}
+//				case 7:
+//				{
+//						  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+//						  printf("７");
+//						  break;
+//				}
+//				case 8:
+//				{
+//						  currentPosition(y, x, csbiInfo.wAttributes);
+//						  printf("８");
+//						  break;
+//				}
+//				default:
+//					break;
+//				}
+//			}
+//			if (mineMap[x][y] == 99)
+//			{
+//				currentPosition(y, x, csbiInfo.wAttributes);
+//				//printf("¤");
+//				printf("■");
+//			}
+//		}
+//		printf("\n");
+//	}
+//}
 
 bool markIndicator(){
 	for (int x = 0; x < 20; x++)
@@ -133,6 +155,10 @@ bool markIndicator(){
 				}
 				else
 				{
+					if (x + xM[n]<0 || x + xM[n]>19 || y + yM[n]<0 || y + yM[n]>25)
+					{
+						continue;
+					}
 					if (mineMap[x + xM[n]][y + yM[n]] == 99)
 					{
 						counter++;     //遍历周围八个格子，有雷计数器+1；
@@ -155,13 +181,13 @@ bool currentPosition(int x, int y, WORD color)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cPos);
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 	CurInfo.dwSize = 1;
-	CurInfo.bVisible = true;
+	CurInfo.bVisible = false;
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CurInfo);
 	return TRUE;
 }
 
 bool isFlaged(int x, int y){
-	if (flagMap[x][y]==1)
+	if (flagMap[x][y] == 1)
 	{
 		return true;
 	}
@@ -169,7 +195,7 @@ bool isFlaged(int x, int y){
 }
 
 bool isMine(int x, int y){
-	if (mineMap[x][y]==99)
+	if (mineMap[x][y] == 99)
 	{
 		return true;
 	}
@@ -177,7 +203,7 @@ bool isMine(int x, int y){
 }
 
 bool isOpen(int x, int y){
-	if (openedMap[x][y]==1)
+	if (openedMap[x][y] == 1)
 	{
 		return true;
 	}
@@ -186,48 +212,203 @@ bool isOpen(int x, int y){
 
 void openSurrand(int x, int y){
 	openedMap[x][y] = 1;
-	for (int n = 1; n < 8;n+=2)
+	printCurrentCell(x, y);
+	for (int n = 1; n < 8; n += 2)
 	{
 		if (x + xM[n]<0 || y + yM[n]<0 || x + xM[n]>19 || y + yM[n]>25)
 		{
 			continue;
 		}
-		if (mineMap[x+xM[n]][y+yM[n]]==0)
+		if (mineMap[x + xM[n]][y + yM[n]] == 0)
 		{
-			openedMap[x + xM[n]][y + yM[n]] = 1;
-			if (!isFlaged(x + xM[n],y + yM[n]))
+			if (!isFlaged(x + xM[n], y + yM[n]))
 			{
-				if (!isOpen(x+xM[n],y+yM[n]))
+				if (!isOpen(x + xM[n], y + yM[n]))
 				{
+					openedMap[x + xM[n]][y + yM[n]] = 1;
+					currentPosition(y + yM[n], x + xM[n], csbiInfo.wAttributes);
+					printf("  ");
 					openSurrand(x + xM[n], y + yM[n]);
 				}
 			}
 		}
-		else if (mineMap[x+xM[n]][y+yM[n]]==99)
+		else if (mineMap[x + xM[n]][y + yM[n]] == 99)
 		{
 			break;
 		}
 		else
 		{
-			openedMap[x + xM[n]][y + yM[n]] = 1;
+			if (!isFlaged(x + xM[n], y + yM[n]))
+			{
+				if (!isOpen(x + xM[n], y + yM[n]))
+				{
+					openedMap[x + xM[n]][y + yM[n]] = 1;
+					printCurrentCell(x + xM[n], y + yM[n]);
+					openSurrand(x + xM[n], y + yM[n]);
+				}
+			}
+
 			break;
 		}
 
 	}
 }
 
-void printOpenedMap(){
-	for (int x = 0; x < 20;x++)
+//测试用的
+// void printOpenedMap(){
+// 	for (int x = 0; x < 20;x++)
+// 	{
+// 		for (int y = 0; y < 26;y++)
+// 		{
+// 			if (openedMap[x][y] == 1)
+// 			{
+// 				printf("  ");
+// 			}
+// 			else
+// 				printf("■");
+// 		}
+// 		printf("\n");
+// 	}
+// }
+
+void gamePlay(){
+	while (true)
 	{
-		for (int y = 0; y < 26;y++)
+		if (!ReadConsoleInput(hStdin, irInBuf, 128, &cNumRead))
 		{
-			if (openedMap[x][y] == 1)
-			{
-				printf("  ");
-			}
-			else
-				printf("■");
+			continue;
 		}
-		printf("\n");
+		for (unsigned int i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].Event.MouseEvent.dwButtonState)
+			{
+			case 1:
+			{
+					  if (isOpen(irInBuf[i].Event.MouseEvent.dwMousePosition.Y, irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2))
+					  {
+						  break;
+					  }
+					  if (isFlaged(irInBuf[i].Event.MouseEvent.dwMousePosition.Y, irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2))
+					  {
+						  break;
+					  }
+					  if (mineMap[irInBuf[i].Event.MouseEvent.dwMousePosition.Y][irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2] == 99)
+					  {
+						  gameOver(irInBuf[i].Event.MouseEvent.dwMousePosition.X, irInBuf[i].Event.MouseEvent.dwMousePosition.Y);
+						  return;
+					  }
+					  else
+					  {
+						  openSurrand(irInBuf[i].Event.MouseEvent.dwMousePosition.Y, irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2);
+						  break;
+					  }
+			}
+			case 2:
+			{
+					  if (isFlaged(irInBuf[i].Event.MouseEvent.dwMousePosition.Y, irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2))
+					  {
+						  flagMap[irInBuf[i].Event.MouseEvent.dwMousePosition.Y][irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2] = 0;
+						  currentPosition(irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2, irInBuf[i].Event.MouseEvent.dwMousePosition.Y, csbiInfo.wAttributes);
+						  printf("■");
+						  break;
+					  }
+					  else
+					  {
+						  flagMap[irInBuf[i].Event.MouseEvent.dwMousePosition.Y][irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2] = 1;
+						  currentPosition(irInBuf[i].Event.MouseEvent.dwMousePosition.X / 2, irInBuf[i].Event.MouseEvent.dwMousePosition.Y, FOREGROUND_RED | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY);
+						  printf("︾");
+						  break;
+					  }
+			}
+			default:
+				break;
+			}
+
+		}
+	}
+}
+
+void gameOver(int x, int y){
+	for (int g = 0; g < 20; g++)
+	{
+		for (int h = 0; h < 26; h++)
+		{
+			if (mineMap[g][h] == 99)
+			{
+				printCurrentCell(g, h);
+			}
+		}
+	}
+	currentPosition(x / 2, y, BACKGROUND_RED | BACKGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+	printf("¤");
+	currentPosition(0, 20, csbiInfo.wAttributes);
+	return;
+}
+
+void printCurrentCell(int x, int y){
+	switch (mineMap[x][y])
+	{
+	case 1:
+	{
+			  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+			  printf("１");
+			  break;
+	}
+	case 2:
+	{
+			  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+			  printf("２");
+			  break;
+	}
+	case 3:
+	{
+			  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+			  printf("３");
+			  break;
+	}
+	case 4:
+	{
+			  currentPosition(y, x, FOREGROUND_RED | FOREGROUND_INTENSITY);
+			  printf("４");
+			  break;
+	}
+	case 5:
+	{
+			  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+			  printf("５");
+			  break;
+	}
+	case 6:
+	{
+			  currentPosition(y, x, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
+			  printf("６");
+			  break;
+	}
+	case 7:
+	{
+			  currentPosition(y, x, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+			  printf("７");
+			  break;
+	}
+	case 8:
+	{
+			  currentPosition(y, x, csbiInfo.wAttributes);
+			  printf("８");
+			  break;
+	}
+	case 99:
+	{
+			   currentPosition(y, x, BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+			   printf("¤");
+			   break;
+	}
+	case 0:
+	{
+			  currentPosition(y, x, csbiInfo.wAttributes);
+			  printf("  ");
+			  break;
+	}
+	default:
+		break;
 	}
 }
